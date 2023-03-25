833d6fea478217310e21f6491ac94d3f69dcf990
znak99 <xhahekcldhkznak0113@gmail.com>
2023-03-26 03:39:36 +0900

    Initial commit

---
+++ Studier/Studier/StudierApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudierApp.swift
+//  Studier
+//
+//  Created by seungwoo on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct StudierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
